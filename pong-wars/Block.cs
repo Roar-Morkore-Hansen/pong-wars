7f140f819ed8d22c41f3efc257b0618ec2f99092
Roar-Morkore-Hansen <psd254@alumni.ku.dk>
2024-01-30 01:48:21 +0100

    added blocks and basic collision

---
+++ pong-wars/Block.cs
@@ -0,0 +1,10 @@
+using System;
+using DIKUArcade.Graphics;
+using DIKUArcade.Entities;
+
+namespace pong_wars {
+    public class Block : Entity {
+        public Block(Shape shape, IBaseImage image) : base(shape, image) {}
+
+    }
+}
